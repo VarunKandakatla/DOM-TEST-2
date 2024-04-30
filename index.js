3b7d963278d9c8fbab531395f73af4128fd3f8db
Varun Kandakatla <varunkumarkandakatla@gmail.com>
2024-04-30 19:26:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+async function Add()
+{
+    const num1 = document.getElementsByTagName('input')[0].value;
+    const num2 = document.getElementsByTagName('input')[1].value;
+    let sol = await fetch('https://dom-test-1.vercel.app/add2?a='+num1+'&b='+num2,{method : "POST"});
+    sol = await sol.json();
+    console.log(sol);
+    document.getElementsByTagName('p')[0].innerHTML = sol.message;
+
+}
