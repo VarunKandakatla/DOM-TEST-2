de9eb8627e84e2353438a1348195c78065647ea9
Varun Kandakatla <varunkumarkandakatla@gmail.com>
2024-04-30 20:05:34 +0530

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ async function Add()
 {
     const num1 = document.getElementsByTagName('input')[0].value;
     const num2 = document.getElementsByTagName('input')[1].value;
-    let sol = await fetch('https://dom-test-1.vercel.app/add2?a='+num1+'&b='+num2,{method : "POST"});
+    let sol = await fetch('https://dom-test-1.vercel.app/add2?a='+num1+'&b='+num2,{
+        method : "POST",
+        headers :{
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
     sol = await sol.json();
     console.log(sol);
     document.getElementsByTagName('p')[0].innerHTML = sol.message;
-
 }
